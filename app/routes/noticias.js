1329d3db153744579ebbfdcd5f13bcbefa5ab0ed
João <joao.rochap03@gmail.com>
2023-02-09 22:50:40 -0300

    Conexão com o banco de dados realizada, começando a integrar dados do banco de dados no ejs

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,27 @@
 module.exports = (app) => {
+
     app.get("/noticias", (req, res) => {
-        res.render("noticias/noticias");
+        var mysql = require('mysql2'); // mysql2 modulo atualizado para o mysql mais atual;
+
+        var connection = mysql.createConnection({ // Criando a conexão com o banco de dados
+            host: 'localhost',
+            user: 'root',
+            password: 'database',
+            database: 'portal_noticias'
+        });
+
+
+        connection.connect((err) => {
+            if(err){
+                console.log('Erro ao conectar ao banco de dados: ' + err.stack);
+                return;
+            }
+
+            console.log("Conectado ao banco de dados com id de conexão " + connection.threadId);
+        });
+
+        connection.query('select * from noticias', (error, result) => {
+            res.render('noticias/noticias', {noticias: result}); //Enviando uma resposta ao servidor do ejs com as informações do banco de dados;
+        });
     });
 }
