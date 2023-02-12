d8dfb32452497177a46b2db6189832833c3f75ae
João <joao.rochap03@gmail.com>
2023-02-11 21:50:29 -0300

    Arquivos de aprendizagem guardadas em diretório teste e conncetion.connect removido

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -3,15 +3,6 @@ module.exports = (app) => {
     app.get("/noticias", (req, res) => {
         const connection = app.config.dbConnection();
 
-        connection.connect((err) => {
-            if(err){
-                console.log('Erro ao conectar ao banco de dados: ' + err.stack);
-                return;
-            }
-
-            console.log("Conectado ao banco de dados com id de conexão " + connection.threadId);
-        });
-
         connection.query('select * from noticias', (error, result) => {
             res.render('noticias/noticias', {noticias: result}); //Enviando uma resposta ao servidor do ejs com as informações do banco de dados;
         });
